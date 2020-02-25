d8616fe6f239dc0a13cd39cdf9167d694a69c5e5
SukandaChanawt1999 <56350041+SukandaChanawt1999@users.noreply.github.com>
2020-02-25 15:34:32 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -2,8 +2,8 @@
 
 
 $API_URL = 'https://api.line.me/v2/bot/message';
-$ACCESS_TOKEN = 'ouhqskdRP/sUP8uwpjAadPDJz6rj1Y3IR0/ZznmHBgsPmYq6Q+hzdEJ4OXgyw/8NaLy6GLAZYYbLhF/7S6i8K07k3yxT0sWcMEa6ixgJ2c0XIOEKRfUEQAsHVi4PbQU4HEk9GOq/cmdR3iRkQE9e5gdB04t89/1O/w1cDnyilFU='; 
-$channelSecret = 'ba6e01c3eb0671a32e7d9fb3dbabd67d';
+$ACCESS_TOKEN = 'leYiyGHgxWCLY9xOJaB5LuIgGFlZkz+XXA8xvf1A+Yej2a6yDZNObjEIHZsJCyaMUuBz3UtXRTdQzd0qWpnxC0xT+bWLIA7NKdOkO5yEyBwRqp4xWiqKCKN/D1pAcmESLJ6vwIT1CTxOxfNGRWomwgdB04t89/1O/w1cDnyilFU='; 
+$channelSecret = '2a053f4c0d7e7446b4b331d47fa5f954';
 
 
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
